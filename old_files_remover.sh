8320198227a9e0e1764945a1049ee0d2014ae35d
shadibadria <shadibadria@gmail.com>
2021-07-04 20:44:31 +0300

    old file deleter

---
+++ old_files_remover.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+#Author: shadi badria
+
+#Description:
+#remove all files in specified folder that have not 
+#been modified within a given days
+
+read -p "please enter the Dir  you want to delete old files in  it : " dir 
+read -p "How many days old ? " days
+
+readarray -t files < <(find $dir -maxdepth 1 -type f -mtime "+$days")
+echo $files[0]
+for file in "${files[@]}"; do
+    rm -i "$file"
+done
